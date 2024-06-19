fd4e091332c2e202be5e12470fe92ab66275036b
devdouglassilva <devdouglassilva@gmail.com>
2024-06-19 16:33:35 +0000

    Feat: Add user input validations Don't allow submit the form if the user input isn't a valid format

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -3,41 +3,84 @@ import './login-form.css';
 
 export interface UserDTO {
   name?: string;
-  email: string;
-  password: string;
+  email?: string;
+  password?: string;
   validated?: boolean;
 }
-export type UserBasicPersonalData = Pick<UserDTO, 'email' | 'password'>;
+export type UserBasicPersonalData = Required<Pick<UserDTO, 'email' | 'password'>>;
 
 export default function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
+  const [showValidationMessage, setShowValidationMessage] = useState(false);
+
   const userData = { email, password } as UserBasicPersonalData;
+
+  onkeydown = (ev) => {
+    if (ev.key === 'Enter') validateInput();
+  };
+
+  const validateInput = (_?: void) => {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const passwordRegex = /^[a-zA-Z0-9]+$/;
+
+    if (emailRegex.test(email.trim())) setValidEmail(true);
+    else setValidEmail(false);
+    if (passwordRegex.test(password.trim()) && password.trim().length >= 7) setValidPassword(true);
+    else setValidPassword(false);
+
+    !validEmail || !validPassword ? setShowValidationMessage(true) : setShowValidationMessage(false);
+  };
 
   return (
     <>
-      <form action='/' className='login-form'>
-        <h1 className='login-form__title'>Bem-vindo(a) d Taqtile!</h1>
+      <form className='login-form'>
+        <h1 className='login-form__title'>Bem-vindo(a) à Taqtile!</h1>
         <div className='login-form__user-input'>
           <fieldset>
-            {/* Should be Refactored */}
-            <label className='input-group'>
+            {/* Can be Refactored */}
+            <label htmlFor='email' className='input-group'>
               Email
-              <input value={email} onChange={(ev) => setEmail(ev.target.value)} type='text' className='input-group__input' />
-              <div className='input-group__content-side'></div>
-              <p className='input-group__informative-message'></p>
+              <input
+                id='email'
+                required
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
+                type='email'
+                className='input-group__input'
+                autoComplete='email'
+              />
+              {showValidationMessage && !validEmail && <p className='input-group__informative-message'>Digite um email válido.</p>}
             </label>
-            <label className='input-group'>
+            <label htmlFor='password' className='input-group'>
               Senha
-              <input value={password} onChange={(ev) => setPassword(ev.target.value)} type='text' className='input-group__input' />
-              <div className='input-group__content-side'></div>
-              <p className='input-group__informative-message'></p>
+              <input
+                id='password'
+                required
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+                type='password'
+                autoComplete='current-password'
+                className='input-group__input'
+              />
+              {showValidationMessage && password.trim().length < 7 && (
+                <p className='input-group__informative-message'>A senha deve ter ao menos 7 caractéres.</p>
+              )}
+              {showValidationMessage && !validPassword && (
+                <p className='input-group__informative-message'>A senha deve ser composta por caractéres alfánumericos.</p>
+              )}
+              {validPassword ? 'true' : 'false'}
+              {validEmail ? 'true' : 'false'}
+              {showValidationMessage ? 'true' : 'false'}
             </label>
           </fieldset>
         </div>
 
-        <button type='submit' className='login-form__submit'>
-           Entrar
+        <button type='submit' onClick={(event) => validateInput(event.preventDefault())} className='login-form__submit'>
+          Entrar
         </button>
       </form>
     </>
