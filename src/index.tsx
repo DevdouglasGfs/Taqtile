3b6407b8e853fd8bfe7983912acd75593173e716
devdouglassilva <devdouglassilva@gmail.com>
2024-06-20 17:14:18 +0000

    Feat: Add ApolloClient top-level configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+export const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	uri: "https://template-onboarding-node-sjz6wnaoia-uc.a.run.app/graphql",
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>
 )
 
