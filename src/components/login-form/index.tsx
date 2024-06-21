cdd41806a30e46cdea958ca5f96f900d9b08b7fe
devdouglassilva <devdouglassilva@gmail.com>
2024-06-21 23:28:41 +0000

    Feat: Add /users route and users page Implements also Apollo Client middleware; Rename variables and remove unused;

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -2,15 +2,12 @@ import { useRef, useState } from 'react';
 import './login-form.css';
 import { useMutation } from '@apollo/client';
 import { LOGIN_MUTATION } from '../../graphql/mutations/loginMutations';
-import { checkLoginStatus, storeLoginToken } from '../../utils/auth';
+import { storeLoginToken } from '../../utils/auth';
 import { UserDto } from '../../types/user';
-import { Navigate } from 'react-router-dom';
 
 export type UserBasicLoginData = Required<Pick<UserDto, 'email'> & { password: string }>;
 
 export default function LoginForm() {
-  if(checkLoginStatus()) Navigate({to: '/users'})
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -28,7 +25,6 @@ export default function LoginForm() {
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const passwordRegex = /^[a-zA-Z0-9]+$/;
 
-    // Make the user input validation removing any spaces in the start and end of the input.
     if (emailRegex.test(email.trim())) setValidEmail(true);
     else setValidEmail(false);
     if (passwordRegex.test(password.trim()) && password.trim().length >= 7) setValidPassword(true);
@@ -41,18 +37,13 @@ export default function LoginForm() {
   const [mutateLogin, { error, loading }] = useMutation(LOGIN_MUTATION)
 
   async function login(_?: void) {
-    /**
-     * Try to login with the user data and clear the local storage
-     * if the user has a token because the user with a valid token
-     * will be redirected to another page without the need to login again.
-     */
     try {
       validateInput() && mutateLogin({
         variables: {
           data: userData
         }
       }).then(data => {
-        storeLoginToken(data.data.login.token) 
+        storeLoginToken(data.data.login.token)
         return data.data
       }).catch(error => console.log(error))
 
