2d99a70d518fbf67b3b49c661e81d91a39b8da53
devdouglassilva <devdouglassilva@gmail.com>
2024-06-23 19:15:46 +0000

    Docs: Add comments explaining code usage Also move the UserBasicLoginData type to user.ts

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,17 @@
-export type UserDto = {
+/**
+ * Should reflect the UserType defined in the GraphQL schema.
+ */
+export interface UserDto {
     id: number,
     name: string,
     phone: string,
     birthDate: Date,
     email: string,
-    role: string
+    role: "user" | "admin"
 }
+
+/**
+ * Type which is used to validate the login data.
+ * Used in the Login page.
+ */
+export type UserBasicLoginData = Required<Pick<UserDto, 'email'> & { password: string }>;
