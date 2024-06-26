4c19f415874bd75aba94285ce2ef897656fef462
devdouglassilva <devdouglassilva@gmail.com>
2024-06-26 03:47:06 +0000

    Chore: Custom react hook useGetUsers useGetUsers will generate a Error if it do not receive arguments

--- src/hooks/useGetUsers.ts
+++ src/hooks/useGetUsers.ts
@@ -14,6 +14,9 @@ const FETCH_USERS = gql(`
 `)
 
 export const useGetUsers = (data?: PageInput) => {
+    if(!data) throw new Error("DATA WAS NOT PROVIDED", {
+        cause: "data is required. data should have a type of PageInput"
+    })
     return useQuery(FETCH_USERS, {
         variables: { data },
         onError: (error) => console.error(error)
