356705cb5834642c036e90f3f3840e3082ddd8ee
devdouglassilva <devdouglassilva@gmail.com>
2024-06-26 03:35:24 +0000

    Ref: Recreate user management  page Rebuilt utilizing components created with styled-components

--- src/pages/users/management/index.tsx
+++ src/pages/users/management/index.tsx
@@ -1,16 +1,26 @@
-import "./management.css";
 import { useState } from "react";
 import CreateUser from "../../../components/create-user";
+import { Heading } from "../../../components/common/heading";
+import { Menu } from "../../../components/common/menu";
+import { Wrapper } from "../../../components/common/wrapper";
+import { Link } from "../../../components/common/link";
+import { Cta } from "../../../components/common/cta";
+
 
 export function UserManagement() {
     const [showModal, setShowModal] = useState(false)
 
     return <>
         <CreateUser open={showModal} setOpen={setShowModal} />
-        <h1 className="user-management-page__title">Controle de Usuário</h1>
-
-        <div className="user-management-page__action-group">
-            <button type="button" onClick={() => setShowModal(true)}>Adicionar Usuário</button>
-        </div>
+        <Wrapper $dir="column" $maxWidth="80cqw" $gap="1rem">
+            <Menu $gap=".5rem">
+                <Heading as={"h2"}>Atalhos rápidos</Heading>
+                <Wrapper as={"nav"} $wrap="wrap" $gap="12px">
+                    <Link $fill $appearAsButton to={'/'}>Início</Link>
+                    <Link $fill $appearAsButton to={'/users'}>Lista de Usuários</Link>
+                </Wrapper>
+            </Menu>
+            <Cta $fill="half" onClick={() => setShowModal(true)}>Adicionar usuário</Cta>
+        </Wrapper>
     </>
 }
