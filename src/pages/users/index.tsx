ff2d2fa3cf86cf2c6afd131373510ae05a9b343a
devdouglassilva <devdouglassilva@gmail.com>
2024-06-26 03:40:58 +0000

    Ref: Recreate base route /users Rebuilt nested components using styled-components

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,21 +1,66 @@
 import { Outlet, Route, Routes } from "react-router-dom"
-import "./list/users-list.css"
 import { UserManagement } from "./management"
 import UsersList from "./list"
+import { SpecificUserPage } from "./user"
+import { ThemeProvider } from "styled-components"
+import { Hero } from "../../components/common/hero"
+import { Heading } from "../../components/common/heading"
+import { Wrapper } from "../../components/common/wrapper"
+import { Menu } from "../../components/common/menu"
+import { Link } from "../../components/common/link"
+
+
+const NavigationLinks = () => {
+    return <>
+        <Wrapper $maxWidth="max(600px, 100%)">
+            <Menu $gap="2rem">
+                <Heading as={"h2"}>Atalhos rápidos</Heading>
+                <Wrapper $wrap="wrap" $gap="12px">
+                    <Link $fill $appearAsButton to={'/users'}>Lista de Usuários</Link>
+                    <Link $fill $appearAsButton to={'/users/management'}>Gestão dos usuários</Link>
+                </Wrapper>
+            </Menu>
+        </Wrapper>
+    </>
+}
+
+export const theme = {
+    colors: {
+        bgBolder: "#030712",
+        bgBold: "#111827",
+        softEmerald: "#D1FAE5",
+        mediumEmerald: "#34D398",
+        link: "#059669",
+        alert: "rgb(200,50,50)"
+    }
+}
 
 export function UsersPage() {
     return <>
-        <Routes>
-            <Route path="/" element={<>
-                <main className="container">
-                    <Outlet />
-                </main>
-            </>
-            }>
-                <Route index element={<UsersList />} />
-                <Route path="list" element={<UsersList />} />
-                <Route path="management" element={<UserManagement />} />
-            </Route>
-        </Routes>
+        <ThemeProvider theme={theme}>
+            <Routes>
+                <Route path="/" element={<>
+                    <main className="container">
+                        <Hero>
+                            <Heading>
+                                <span>Dashboard</span>
+                                Taqtile
+                            </Heading>
+                        </Hero>
+                        <Outlet />
+                    </main>
+                </>
+                }>
+                    <Route index element={<>
+                        <NavigationLinks />
+                    </>} />
+                    <Route path="users" element={<><Outlet /></>}>
+                        <Route index element={<UsersList />} />
+                        <Route path="management" element={<UserManagement />} />
+                        <Route path="user/:id" element={<SpecificUserPage />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </ThemeProvider>
     </>
 }
