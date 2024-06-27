b28b5c84f4afa710bba050d493ec8f43b6d0e5da
devdouglassilva <devdouglassilva@gmail.com>
2024-06-27 14:56:53 +0000

    Chore: Improve UI for error page

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,21 +1,17 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 export default function ErrorPage() {
-    const error = useRouteError() as Error;
+    const error = useRouteError();
     console.log(error)
 
     return (
         <div className="error-page">
-            <h1 className="error-page__title">Um erro aconteceu</h1>
-            <p className="error-page__subtitle">Desculpe, um erro inesperado aconteceu.</p>
-
-            <details className="error-page__wrap-details">
-                <summary className="error-page__summary">Detalhes</summary>
-                <p className="error-page__details">{
-                    //@ts-ignore
-                    error?.statusText || error.message
-                }</p>
-            </details>
+            <h1 className="error-page__title">
+                {isRouteErrorResponse(error) ? error.status : 500}
+            </h1>
+            <p className="error-page__message">
+                {isRouteErrorResponse(error) && error.status === 404 && "Página não encontrada."}
+            </p>
         </div>
     )
 }
