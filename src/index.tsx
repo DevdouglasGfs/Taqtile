37b552dd29f3b1d3ed0515fb668318b40584de87
devdouglassilva <devdouglassilva@gmail.com>
2024-07-02 19:03:42 +0000

    Chore: Remove unrelevant comments

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,7 @@ export const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('access-token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
